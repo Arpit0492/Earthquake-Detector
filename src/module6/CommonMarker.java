baa1e0e0deb5c189e63324b0b4ae82d5e90f623e
Arpit Johri <johri.arpit@gmail.com>
2016-11-15 09:26:15 +0530

    Update CommonMarker.java

--- src/module6/CommonMarker.java
+++ src/module6/CommonMarker.java
@@ -6,9 +6,6 @@ import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import processing.core.PGraphics;
 
 /** Implements a common marker for cities and earthquakes on an earthquake map
- * 
- * @author UC San Diego Intermediate Software Development MOOC team
- *
  */
 public abstract class CommonMarker extends SimplePointMarker {
 
@@ -34,8 +31,7 @@ public abstract class CommonMarker extends SimplePointMarker {
 	}
 	
 	// Common piece of drawing method for markers; 
-	// YOU WILL IMPLEMENT. 
-	// Note that you should implement this by making calls 
+	// Note that we should implement this by making calls 
 	// drawMarker and showTitle, which are abstract methods 
 	// implemented in subclasses
 	public void draw(PGraphics pg, float x, float y) {
