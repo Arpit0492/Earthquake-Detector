408e23c61c71ab753ecf0bfbee0851c5bd76a14e
Arpit Johri <johri.arpit@gmail.com>
2016-11-15 09:33:08 +0530

    Update OceanQuakeMarker.java

--- src/module6/OceanQuakeMarker.java
+++ src/module6/OceanQuakeMarker.java
@@ -4,9 +4,6 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 
 /** Implements a visual marker for ocean earthquakes on an earthquake map
- * 
- * @author UC San Diego Intermediate Software Development MOOC team
- *
  */
 public class OceanQuakeMarker extends EarthquakeMarker {
 	
@@ -24,8 +21,6 @@ public class OceanQuakeMarker extends EarthquakeMarker {
 		// DO NOT set the fill color.  That will be set in the EarthquakeMarker
 		// class to indicate the depth of the earthquake.
 		// Simply draw a centered square.
-		// HINT: Notice the radius variable in the EarthquakeMarker class
-		// and how it is set in the EarthquakeMarker constructor
 		pg.rect(x-radius, y-radius, 2*radius, 2*radius);
 	}
 	
